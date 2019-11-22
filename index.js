19a4d1daa3e858258131ef6e2a2a537c94b20c2a
Deval Shah <deval.shah@zymr.com>
2019-11-22 12:16:09 +0530

    remove unneeded pacakge

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 #!/usr/bin/env node
 
 const yargs = require("yargs");
-const axios = require("axios");
-const _ = require("lodash");
-const moment = require('moment');
 const movieRecommendation = require('./movie-recommendation');
 
 const options = yargs
- .scriptName("movie-recommendation")
  .usage("Usage: $0 -genre <genre> -time <time>")
  .option("genre", { alias: "genre", describe: "genre", type: "string", demandOption: true })
  .option("time", { alias: "time", describe: "time", type: "string" , demandOption: true })
